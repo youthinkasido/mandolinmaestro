c11b10155b62d03fb5cad91e31e15ca725aced69
Arshia <youthinkasido@gmail.com>
2019-09-15 15:07:01 -0400

    Add webpack to package json

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.static(__dirname))
+
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'home.html'));
 });
 
 app.listen(process.env.PORT || 4000, function () {
